7af94c993c87e513606a592a4c3d509a1be0a49d
cllasserre <clementlasserre00@gmail.com>
2022-03-15 11:43:31 +0100

    Création de la fonction play() dans simulation pour lancer le jeu, ajout de la possibilité de créer un batiment et de créer un colon

--- Colony/Colony/Simulation.cs
+++ Colony/Colony/Simulation.cs
@@ -8,20 +8,17 @@ namespace Colony
 {
     class Simulation
     {
-        private List<Settler> _settlers; 
-        private List<Building> _buildings = new List<Building> { };
         private Village _village;
-
+        private static int _turn = 0;
+        private int _turnNb;
+        
         public Simulation()
         {
-            Builder s1 = new Builder();
-            Builder s2 = new Builder();
-            Builder s3 = new Builder();
-            Builder s4 = new Builder();
-            _settlers = new List<Settler> { s1, s2, s3, s4 };
-            _village = new Village(4);
+            _turnNb = _turn; 
+            _village = new Village();
         }
 
+        /*
         public override string ToString()
         {
 
@@ -31,12 +28,99 @@ namespace Colony
                 retour += "Les batiments existants sont les suivants : \n";
             foreach(Building b in _buildings)
                 retour += b.ToString();
-            if (_settlers.Count() != 0)
+            if (_village.getSettlers().Count() != 0)
                 retour += "Les colons existants sont les suivants : \n";
-            foreach (Settler s in _settlers)
+            foreach (Settler s in _village.getSettlers())
                 retour += s.ToString();
             retour += "\n";
             return retour;
+        }*/
+
+        public void Play()
+        {
+            Console.WriteLine("Vous êtes au tour {0} \n Vous pouvez : ", _turnNb);
+            Console.WriteLine("Créer un Batiment (entrez 1)");
+            string createSettler = _village.freePlaces()?"Créer un Colon (entrez 2)":"Créer un Colon (vous n'avez pas assez d'infrastructures pour accueillir de nouveaux colons)";
+            Console.WriteLine(createSettler);
+            int play = int.Parse(Console.ReadLine());
+            switch (play)
+            {
+                case 1:
+                    addBuilding();
+                    break;
+                case 2:
+                    if (_village.freePlaces())
+                    {
+                        addSettler();
+                    }
+                    else
+                    {
+                        Console.WriteLine("vous n'avez pas assez d'infrastructures pour accueillir de nouveaux colons");
+                        Play();
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Votre réponse n'est pas valide");
+                    Play();
+                    break;
+            }
+            
+
+        }
+
+        public void addBuilding() {
+            Console.WriteLine("Entrez la ligne de l'angle en haut à gauche de votre batiment");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez la colonne de l'angle en haut à gauche de votre batiment");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez 1 pour créer un Hotel");
+            Console.WriteLine("Entrez 2 pour créer un Restaurant");
+            Console.WriteLine("Entrez 3 pour créer une Infrastructure Sportive");
+            int create = int.Parse(Console.ReadLine());
+            switch (create)
+            {
+                case 1:
+                    Hotel hotel = new Hotel(x,y);
+                    _village.addBuildings(hotel);
+                    break;
+                case 2:
+                    Restaurant restaurant = new Restaurant(x, y);
+                    _village.addBuildings(restaurant);
+                    break;
+                case 3:
+                    SportsInfrastructure sportInfrastructure = new SportsInfrastructure(x, y);
+                    _village.addBuildings(sportInfrastructure);
+                    break;
+            }
+
+        }
+        private void addSettler() {
+            Console.WriteLine("Entrez 1 pour créer un Batisseur");
+            Console.WriteLine("Entrez 2 pour créer un Sportif");
+            Console.WriteLine("Entrez 3 pour créer un Coach");
+            int create = int.Parse(Console.ReadLine());
+            switch (create)
+            {
+                case 1:
+                    Builder builder = new Builder();
+                    _village.addSettler(builder);
+                    break;
+                case 2:
+                    createAthletics();
+                    break;
+                case 3:
+                    Coach coach = new Coach();
+                    _village.addSettler(coach);
+                    break;
+            }
+        }
+
+        private void createAthletics()
+        {
+            Console.WriteLine("Entrez votre nationnalité");
+            string nationality = Console.ReadLine();
+            Athletic athletic = new Athletic("Française", "natation"); //TO DO choisir le sport
+            _village.addSettler(athletic);
         }
     }
 }
--- Colony/Colony/Village.cs
+++ Colony/Colony/Village.cs
@@ -8,17 +8,84 @@ namespace Colony
 {
     class Village
     {
+        private List<Building> _buildings = new List<Building> { };
         private int _maxNbSettlers;
         private string[,] _gameBoard = new string[20, 40];
+        private List<Settler> _settlers;
 
-        public Village(int maxNbSettlers)
+        public Village()
         {
-            _maxNbSettlers = maxNbSettlers;
+            Builder s1 = new Builder();
+            Builder s2 = new Builder();
+            Builder s3 = new Builder();
+            Builder s4 = new Builder();
+            _settlers = new List<Settler> { s1, s2, s3, s4 };
+            _maxNbSettlers = 4;
         }
 
         public override string ToString()
         {
-            return "Le village peut contenir jusqu'à : " + _maxNbSettlers + " colons \nEt le plateau de jeu contient : " + _gameBoard.GetLength(0) + " lignes et " + _gameBoard.GetLength(1) + " colonnes \n";
+            return "a faire";
         }
+
+        public List<Settler> getSettlers()
+        {
+            return _settlers;
+        }
+
+        public void addBuildings(Building building)
+        {
+            _buildings.Add(building);
+        }
+
+        public void addSettler(Settler settler)
+        {
+            _settlers.Add(settler);
+
+        }
+
+        public int freeHotelPlaces()
+        {
+            int places = 0;
+            foreach (Hotel hotel in _buildings)
+            {
+                foreach (Settler settler in hotel.getSettlers())
+                {
+                    if (settler is null)
+                    {
+                        places++;
+                    }
+                }
+            }
+            return places;
+        }
+
+
+        public int freeRestaurantPlaces()
+        {
+            int places = 0;
+            foreach (Restaurant restaurant in _buildings)
+            {
+                foreach (Settler settler in restaurant.getSettlers())
+                {
+                    if (settler is null)
+                    {
+                        places++;
+                    }
+                }
+            }
+            return places;
+        }
+
+        public int placesNb()
+        {
+            return Math.Min(freeRestaurantPlaces(),freeHotelPlaces());
+        }
+        public bool freePlaces()
+        {
+            return placesNb() != 0;
+        }
+
+
     }
 }
